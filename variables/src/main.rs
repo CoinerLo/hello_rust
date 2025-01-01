4a79a997cedee43682980ae8aa838060a5603796
CoinerLo <coiner4677@gmail.com>
2025-01-01 20:56:27 +0300

    create variables

---
+++ variables/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+
+    let mut y = 5;
+    println!("The value of y is: {y}");
+    y = 6;
+    println!("The value of y is: {y}");
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("{}", spaces);
+}
