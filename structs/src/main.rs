e64c0f39d554ebced6de6f6bd8f0df2b4fa3e04e
CoinerLo <coiner4677@gmail.com>
2025-01-06 20:24:02 +0300

    update structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -6,6 +6,14 @@ struct User {
     sign_in_count: u64,
 }
 
+#[derive(Debug)]
+struct Color(i32, i32, i32);
+#[derive(Debug)]
+struct Point(i32, i32, i32);
+
+#[derive(Debug)]
+struct AlwaysEqual;
+
 fn main() {
     let mut user1 = User {
         active: true,
@@ -29,6 +37,15 @@ fn main() {
     println!("user3 = {:?}",user3);
     println!("user1.active = {}", user1.active);
     println!("user1.sign_in_count = {}", user1.sign_in_count);
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+    let subject = AlwaysEqual;
+    println!("black.0 = {:?}", black.0);
+    println!("black.1 = {:?}", black.1);
+    println!("black.2 = {:?}", black.2);
+    println!("origin = {:?}", origin);
+    println!("subject = {:?}", subject);
 }
 
 fn build_user(email: String, username: String) -> User {
