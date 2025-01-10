ea1e3d059ee61b024e0d05556c2b234a2243b9ca
CoinerLo <coiner4677@gmail.com>
2025-01-10 21:15:05 +0300

    update restaurant. add back of house

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,6 +1,6 @@
 mod front_of_house {
     pub mod hosting {
-        pub fn add_to_waitlist {}
+        pub fn add_to_waitlist() {}
         fn seat_at_table() {}
     }
 
@@ -12,13 +12,37 @@ mod front_of_house {
 }
 
 pub fn eat_at_restaurant() {
-    create::front_of_house::hosting::add_to_waitlist();
+    crate::front_of_house::hosting::add_to_waitlist();
 
     front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+    // meal.seasonal_fruit = String::from("blueberries"); // будет ошибка, так как свойство не публичное
 }
 
 fn deliver_order() {}
 
 mod back_of_house {
-    
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast{
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peachs"),
+            }
+        }
+    }
 }
