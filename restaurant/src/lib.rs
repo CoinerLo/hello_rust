486c8217cdf447350860b3b64c70a80c80a2bd65
CoinerLo <coiner4677@gmail.com>
2025-01-10 21:27:00 +0300

    update restaurant. add aliases

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -11,15 +11,37 @@ mod front_of_house {
     }
 }
 
-pub fn eat_at_restaurant() {
-    crate::front_of_house::hosting::add_to_waitlist();
+use crate::front_of_house::hosting;
+use crate::front_of_house::hosting::add_to_waitlist; // так лучше не делать, так как непонятно потом откуда в итоге берется функция
+use std::collections::HashMap; // Но подключать структуры таким образом правильно
 
-    front_of_house::hosting::add_to_waitlist();
+use std::fmt::Result;
+use std::io::Result as IoResult; // пример альяса, как задать своё имя при импорте
+// fn function1() -> Result {
+//     // --snip--
+// }
+
+// fn function2() -> IoResult<()> {
+//     // --snip--
+// }
+
+pub fn eat_at_restaurant() {
+    crate::front_of_house::hosting::add_to_waitlist(); // это абсолютная ссылка, она наичнается с корневого каталога
+    front_of_house::hosting::add_to_waitlist(); // это относительная ссылка, мы находимся с front_of_house на одном уровне и с него начинается отсчет
+    hosting::add_to_waitlist(); // такой вызов становится возможным благодаря use
+    add_to_waitlist();  // так лучше не делать, так как непонятно потом откуда в итоге берется функция
+
+    let mut map = HashMap::new();
+    map.insert(1, 2);
 
     let mut meal = back_of_house::Breakfast::summer("Rye");
     meal.toast = String::from("Wheat");
     println!("I'd like {} toast please", meal.toast);
     // meal.seasonal_fruit = String::from("blueberries"); // будет ошибка, так как свойство не публичное
+
+    // enum всегда целиком становится публичным, структура и прочее становится публичным по частям, см. выше
+    let order1 = back_of_house::Appetizer::Salat;
+    let order2 = back_of_house::Appetizer::Soup;
 }
 
 fn deliver_order() {}
@@ -44,5 +66,10 @@ mod back_of_house {
                 seasonal_fruit: String::from("peachs"),
             }
         }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salat,
     }
 }
