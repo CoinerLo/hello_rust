45d453c04e43d65e39d98ccc95cef413ac687faf
CoinerLo <coiner4677@gmail.com>
2025-01-15 21:52:27 +0300

    update errors v2

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -35,6 +35,8 @@ fn main() {
         .expect("hello.txt should be included in this project");
     
     let _ = read_username_from_file();
+    let _ = read_username_from_file_with_operator();
+    let _ = read_username_from_file_with_operatorv2();
 }
 
 // Проброс ошибок
@@ -51,4 +53,18 @@ fn read_username_from_file() -> Result<String, io::Error> {
         Ok(_) => Ok(username),
         Err(e) => Err(e),
     }
+}
+
+// Оператор ?
+fn read_username_from_file_with_operator() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+fn read_username_from_file_with_operatorv2() -> Result<String, io::Error> {
+    let mut username = String::new();
+    File::open("text.txt")?.read_to_string(&mut username)?;
+    Ok(username)
 }
