a298b5b1c632750972362a1b4277a7ee9e63c15f
CoinerLo <coiner4677@gmail.com>
2025-01-18 21:45:47 +0300

    update traits

--- traits/src/lib.rs
+++ traits/src/lib.rs
@@ -1,5 +1,13 @@
+// когда требуется обязательная имплементация метода в структуре которая реализуется этот типаж
 pub trait Summary {
     fn summarize(&self) -> String;
+}
+
+// имплементация метода по дефолту
+pub trait Summary_def {
+    fn summarize(&self) -> String {
+        String::from("(Read more...)")
+    }
 }
 
 pub struct NewsArticle {
@@ -28,7 +36,30 @@ impl Summary for Tweet {
     }
 }
 
+pub struct SMS {
+    pub content: String,
+    pub number: String,
+}
+
+impl Summary_def for SMS {}
+
+pub fn test_run() {
+    let mut _i = Tweet {
+        username: String::from("My name"),
+        content: String::from("Hello my content"),
+        reply: false,
+        retweet: false,
+    };
+    println!("call summarize = {}", _i.summarize());
+
+    let mut _sms = SMS {
+        content: String::from("Hello my sms"),
+        number: String::from("+7999"),
+    };
+    println!("call default summarize = {}", _sms.summarize());
+}
+
 #[cfg(test)]
 mod tests {
-    
+
 }
