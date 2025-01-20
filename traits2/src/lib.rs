3441130d0a7ff184324fa9213af745e39d556429
CoinerLo <coiner4677@gmail.com>
2025-01-20 21:52:21 +0300

    update traits2

--- traits2/src/lib.rs
+++ traits2/src/lib.rs
@@ -1,4 +1,4 @@
-use std::fmt::Display;
+use std::fmt::{Debug, Display};
 
 pub trait Summary {
     fn summarize_author(&self) -> String;
@@ -21,7 +21,40 @@ pub fn notify2<T: Summary>(item: &T) { // ограничение типажа - 
 // pub fn notify<T: Summary>(item1: &T, item2: &T) {
 
 // Несколько границ типажей для одного параметра
-pub fn notify(item: &(impl Summary + Display)) {}
+// pub fn notify(item: &(impl Summary + Display)) {}
+// pub fn notify<T: Summaty + Display>(item: &T) {}
+
+// Множество параметров с множеством границ типажей
+// fn some_function<T: Display + Clone, U: Clone + Debug>(t: &T, u: &U) {}
+
+fn some_function<T, U>(t: &T, u: &U) -> i32
+where 
+    T: Display + Clone,
+    U: Clone + Debug,
+{
+    32
+}
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
