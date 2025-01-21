01a5c447603aa7f7754307f5b631c101d28272bd
CoinerLo <coiner4677@gmail.com>
2025-01-21 21:59:22 +0300

    update links

--- links/src/main.rs
+++ links/src/main.rs
@@ -1,3 +1,11 @@
+fn longest_first<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
 fn longest<'a>(x: &'a str, y: &str) -> &'a str {
     println!("{y}");
     x
@@ -8,4 +16,9 @@ fn main() {
     let y = "y";
     let z = longest(x, y);
     println!("x = {}, z = {}", x, z);
+
+    let t = "t";
+    let p = "p";
+    let w = longest_first(t, p);
+    println!("w = {w}");
 }
