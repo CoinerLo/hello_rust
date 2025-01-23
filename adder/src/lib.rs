2b8acd6b776d45038965bc9531673a178e61226b
CoinerLo <coiner4677@gmail.com>
2025-01-23 21:30:47 +0300

    update tests v2

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -45,6 +45,15 @@ mod tests {
         let result = greeting("Carol");
         assert!(result.contains("Carol"));
     }
+
+    #[test]
+    fn greeting_contains_name_with_description() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{result}`"
+        );
+    }
 }
 
 // cargo test
