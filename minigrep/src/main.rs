a4d3ba8b91e5db3aaf91df4ea5963f27f3eb9f90
CoinerLo <coiner4677@gmail.com>
2025-01-26 17:49:10 +0300

    update minigrep v4

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,6 @@
-use std::{env, fs, process};
+use std::{env, process};
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,25 +12,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let context = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
-    println!("With text:\n{context}");
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    };
 }
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2]. clone();
-
-        Ok(Config { query, file_path })
-    }
-}
-
