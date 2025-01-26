80cdd818fd4a671919a0b20949f17a54e092e6d5
CoinerLo <coiner4677@gmail.com>
2025-01-26 10:42:36 +0300

    update minigrep v2

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,12 +3,26 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    let config = Config::new(&args);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let context = fs::read_to_string(file_path)
+    let context = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
     println!("With text:\n{context}");
 }
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        Config {
+            query: args[1].clone(),
+            file_path: args[2].clone(),
+        }
+    }
+}
+
