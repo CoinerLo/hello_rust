c64f46bfc465eed4b5adeb0e64ec0b1a56ec0d89
CoinerLo <coiner4677@gmail.com>
2025-01-31 21:32:03 +0300

    update iterators

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -4,4 +4,24 @@ fn main() {
     for val in v1_iter {
         println!("val = {val}");
     }
+
+    // типаж Iterator необходимо реализовывать структурам для возможности итерации по ним
+    pub trait Iterator {
+        type Item;
+
+        fn next(&mut self) -> Option<Self::Item>;
+    }
+}
+
+// цикл работы итератора
+#[test]
+fn iterator_demonstration() {
+    let v1 = vec![1, 2, 3];
+
+    let mut v1_iter = v1.iter();
+
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(), None);
 }
