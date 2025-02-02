3be66a0787d62a986002f33df47e2fece7e77319
CoinerLo <coiner4677@gmail.com>
2025-02-02 19:35:58 +0300

    add build profile

---
+++ build_profile/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// Разные профили для сборки
+// Cargo.toml
+// [profile.dev]
+// opt-level = 0
+
+// [profile.release]
+// opt-level = 3
+
+// [profile.*]
+// общие настройки для все сборок
+
