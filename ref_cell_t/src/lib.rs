1ca2814e2c83542982734f6ccd8fcb58f6fe52c0
CoinerLo <coiner4677@gmail.com>
2025-02-06 20:46:48 +0300

    update ref_cell_t v2

--- ref_cell_t/src/lib.rs
+++ ref_cell_t/src/lib.rs
@@ -40,11 +40,35 @@ where T: Messenger
 
 #[cfg(test)]
 mod tests {
+    use std::cell::RefCell;
+
     use super::*;
 
+    struct MockMessenger {
+        send_messages: RefCell<Vec<String>>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {
+                send_messages: RefCell::new(vec![]),
+            }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            self.send_messages.borrow_mut().push(String::from(message));
+        }
+    }
+
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTrackeer::new(&mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+
+        assert_eq!(mock_messenger.send_messages.borrow().len(), 1);
     }
 }
