00de444e09b599eb9c8c2b130dbe59e4213a5c21
CoinerLo <coiner4677@gmail.com>
2025-02-06 20:38:44 +0300

    update ref_cell_t

--- ref_cell_t/src/lib.rs
+++ ref_cell_t/src/lib.rs
@@ -38,6 +38,13 @@ where T: Messenger
     }
 }
 
-fn main() {
-    println!("Hello, world!");
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
 }
