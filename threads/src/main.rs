67bd632b88c6f7c78a6f253a85d6e927e5a93c23
CoinerLo <coiner4677@gmail.com>
2025-02-09 20:24:09 +0300

    update threads

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -1,8 +1,9 @@
 use std::{thread, time::Duration};
 
 fn main() {
-    thread::spawn(|| {
-        for i in 1..10 {
+    let v = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let handle = thread::spawn(move || {
+        for i in v {
             println!("hi number {i} from the spawned thread!");
             thread::sleep(Duration::from_millis(1))
         }
@@ -12,4 +13,6 @@ fn main() {
         println!("hi number {i} from the main thread!");
         thread::sleep(Duration::from_millis(1));
     }
+
+    handle.join().unwrap();
 }
