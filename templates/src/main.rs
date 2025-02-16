f865c34461fb73d95f0697566385cf09a1aa1387
CoinerLo <coiner4677@gmail.com>
2025-02-16 17:24:26 +0300

    update templates v5

--- templates/src/main.rs
+++ templates/src/main.rs
@@ -60,6 +60,20 @@ fn main() {
         3 => println!("three"),
         _ => println!("anything"),
     }
+
+    // ..= сравнение с диапазоном значений - только для char и чисел
+    let x = 5;
+    match x {
+        1..=5 => println!("one through five"),
+        _ => println!("something else"),
+    }
+
+    let x = 'c';
+    match x {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("somthing alse"),
+    }
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
