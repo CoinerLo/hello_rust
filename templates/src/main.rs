f098e02be90681d9222403a91a823c6e481fdcc4
CoinerLo <coiner4677@gmail.com>
2025-02-17 21:02:53 +0300

    update templates v7

--- templates/src/main.rs
+++ templates/src/main.rs
@@ -90,6 +90,30 @@ fn main() {
         Point { x: 0, y } => println!("On the y axis at {y}"),
         Point { x, y } => println!("On neither axist: ({x}, {y})"),
     }
+
+    // деструктуризация перечислений
+    let msg = Message::ChangeColor(0, 160, 255);
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no date to destructure.");
+        }
+        Message::Move { x, y } => {
+            println!("Move in the x direction {x} and in the y direction {y}");
+        }
+        Message::Write(text) => {
+            println!("Text message: {text}");
+        }
+        Message::ChangeColor(r, g, b) => {
+            println!("Change the color to red {r}, green {g}, and blue {b}");
+        }
+    }
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
 }
 
 struct Point {
