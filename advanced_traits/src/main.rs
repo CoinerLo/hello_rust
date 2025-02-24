fd8bb5d4b7807102dc9d803ae003bde7c468418b
CoinerLo <coiner4677@gmail.com>
2025-02-24 21:01:40 +0300

    update advanced_traits v2

--- advanced_traits/src/main.rs
+++ advanced_traits/src/main.rs
@@ -19,3 +19,8 @@ impl Add for Point {
 fn main() {
     assert_eq!(Point { x: 2, y:3 } + Point { x: 1, y: 0}, Point { x: 3, y: 3 });
 }
+
+// trait Add<Rhs=Self> {
+//     type Output;
+//     fn add(self, rhs: Rhs) -> Self::Output;
+// }
