956f3ca056c97133175db825ffa57ec34f293276
CoinerLo <coiner4677@gmail.com>
2025-02-24 21:05:53 +0300

    update advanced_traits v3

--- advanced_traits/src/main.rs
+++ advanced_traits/src/main.rs
@@ -24,3 +24,13 @@ fn main() {
 //     type Output;
 //     fn add(self, rhs: Rhs) -> Self::Output;
 // }
+
+struct Millimeters(u32);
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+    fn add(self, other: Meters) -> Millimeters {
+        Millimeters(self.0 + (other.0 * 1000))
+    }
+}
