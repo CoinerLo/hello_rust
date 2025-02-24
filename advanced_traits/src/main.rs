62dcdd83a4fc4f62c0463057d09ba05bc0d48bc4
CoinerLo <coiner4677@gmail.com>
2025-02-24 20:59:15 +0300

    update advanced_traits

--- advanced_traits/src/main.rs
+++ advanced_traits/src/main.rs
@@ -1,15 +1,21 @@
-pub trait Iterator<T> {
-    type Item;
-    fn next(&mut self) -> Option<Self::Item>;
+use std::ops::Add;
+
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
 }
 
-impl Iterator for Counter {
-    type Item = u32;
-    fn next(&mut self) -> Option<Self::Item> {
-
-    };
+impl Add for Point {
+    type Output = Point;
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
 }
-
 fn main() {
-    println!("Hello, world!");
+    assert_eq!(Point { x: 2, y:3 } + Point { x: 1, y: 0}, Point { x: 3, y: 3 });
 }
