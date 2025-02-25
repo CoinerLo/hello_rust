13bb65cf322036038a20f5865c747c538efa3c22
CoinerLo <coiner4677@gmail.com>
2025-02-25 20:50:28 +0300

    update advanced_traits v5

--- advanced_traits/src/main.rs
+++ advanced_traits/src/main.rs
@@ -20,7 +20,11 @@ fn main() {
     assert_eq!(Point { x: 2, y:3 } + Point { x: 1, y: 0}, Point { x: 3, y: 3 });
 
     let person = Human;
+    Pilot::fly(&person);
+    Wizard::fly(&person);
     person.fly();
+
+    println!("A baby dog is called a {}", Dog::baby_name());
 }
 
 // trait Add<Rhs=Self> {
@@ -65,3 +69,23 @@ impl Human {
         println!("*waving arms furiously*");
     }
 }
+
+trait Animal {
+    fn baby_name() -> String;
+}
+
+struct  Dog;
+
+impl Dog {
+    fn baby_name() -> String {
+        String::from("Spot")
+    }
+}
+
+impl Animal for Dog {
+    fn baby_name() -> String {
+        String::from("puppy")
+    }
+}
+
+
