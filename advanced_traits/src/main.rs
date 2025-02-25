5a4c343e1d822404bef4feade4f7c66458f55248
CoinerLo <coiner4677@gmail.com>
2025-02-25 20:41:25 +0300

    update advanced_traits v4

--- advanced_traits/src/main.rs
+++ advanced_traits/src/main.rs
@@ -18,6 +18,9 @@ impl Add for Point {
 }
 fn main() {
     assert_eq!(Point { x: 2, y:3 } + Point { x: 1, y: 0}, Point { x: 3, y: 3 });
+
+    let person = Human;
+    person.fly();
 }
 
 // trait Add<Rhs=Self> {
@@ -32,5 +35,33 @@ impl Add<Meters> for Millimeters {
     type Output = Millimeters;
     fn add(self, other: Meters) -> Millimeters {
         Millimeters(self.0 + (other.0 * 1000))
+    }
+}
+
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard{
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!");
+    }
+}
+
+impl Human {
+    fn fly(&self) {
+        println!("*waving arms furiously*");
     }
 }
