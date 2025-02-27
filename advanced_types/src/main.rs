a198d8ab130918f84c23da36b97cec202263b948
CoinerLo <coiner4677@gmail.com>
2025-02-27 20:58:34 +0300

    update advanced_types

--- advanced_types/src/main.rs
+++ advanced_types/src/main.rs
@@ -1,8 +1,15 @@
 type Kilometers = i32;
 
+type Thunk = Box<dyn Fn() + Send + 'static>;
 
 fn main() {
     let x: i32 = 5;
     let y: Kilometers = 5;
-    println!("x + y = ", x + y);
+    println!("x + y = {}", x + y);
+
+    let _f: Thunk = Box::new(|| println!("hi"));
+    fn _takes_long_type(_f: Thunk) {}
+    fn _returns_long_type() -> Thunk {
+        Box::new(|| println!("by"))
+    }
 }
