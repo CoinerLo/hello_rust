0d19614a656da3ca07dceef619672badea574b28
CoinerLo <coiner4677@gmail.com>
2025-02-27 21:06:34 +0300

    update advanced_types v2

--- advanced_types/src/main.rs
+++ advanced_types/src/main.rs
@@ -1,6 +1,10 @@
+use core::fmt;
+
 type Kilometers = i32;
 
 type Thunk = Box<dyn Fn() + Send + 'static>;
+
+type Result<T> = std::result::Result<T, std::io::Error>;
 
 fn main() {
     let x: i32 = 5;
@@ -12,4 +16,11 @@ fn main() {
     fn _returns_long_type() -> Thunk {
         Box::new(|| println!("by"))
     }
+}
+
+pub trait Write {
+    fn write(&mut self, buf: &[u8]) -> Result<usize>;
+    fn flush(&mut self) -> Result<()>;
+    fn write_all(&mut self, buf: &[u8]) -> Result<()>;
+    fn write_fmt(&mut self, fmt: fmt::Arguments) -> Result<()>;
 }
