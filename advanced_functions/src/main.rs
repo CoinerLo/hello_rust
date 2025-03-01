d39a4eaa75792a0d5a3f517c5b338f6affe2ddce
CoinerLo <coiner4677@gmail.com>
2025-03-01 16:14:53 +0300

    update advanced_functions

--- advanced_functions/src/main.rs
+++ advanced_functions/src/main.rs
@@ -9,4 +9,10 @@ fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
 fn main() {
     let answer = do_twice(add_one, 5);
     println!("The answer is: {answer}");
+
+    let list_of_numbers = vec![1, 2, 3];
+    let _list_of_strings: Vec<String> = list_of_numbers.iter().map(|i| i.to_string()).collect();
+
+    let list_of_numbers = vec![1, 2, 3];
+    let _list_of_strings: Vec<String> = list_of_numbers.iter().map(ToString::to_string).collect();
 }
