0dd297914398f823778c6eb363a5c6014ada4c49
CoinerLo <coiner4677@gmail.com>
2025-03-01 16:29:17 +0300

    update advanced_functions v2

--- advanced_functions/src/main.rs
+++ advanced_functions/src/main.rs
@@ -6,6 +6,11 @@ fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
     f(arg) + f(arg)
 }
 
+enum Status {
+    Value(u32),
+    Stop,
+}
+
 fn main() {
     let answer = do_twice(add_one, 5);
     println!("The answer is: {answer}");
@@ -15,4 +20,25 @@ fn main() {
 
     let list_of_numbers = vec![1, 2, 3];
     let _list_of_strings: Vec<String> = list_of_numbers.iter().map(ToString::to_string).collect();
+
+    let _list_of_statuses: Vec<Status> = (0u32..20).map(Status::Value).collect();
+
+    // возврат замыкания
+    let handlers = vec![returns_closure(), returns_initialized_closure(123)];
+    for handler in handlers {
+        let output = handler(5);
+        println!("{output}");
+    }
+}
+
+fn _returns_closure() -> impl Fn(i32) -> i32 {
+    |x| x + 1
+}
+
+fn returns_closure() -> Box<dyn Fn(i32) -> i32> {
+    Box::new(|x| x + 1)
+}
+
+fn returns_initialized_closure(init: i32) -> Box<dyn Fn(i32) -> i32> {
+    Box::new(move |x| x + init)
 }
