6b5165204cb8ad4192520ca305e47ac9bf19a9c6
CoinerLo <coiner4677@gmail.com>
2025-03-02 12:44:41 +0300

    add hello_macro_derive

---
+++ hello_macro/hello_macro_derive/src/lib.rs
@@ -0,0 +1,39 @@
+use proc_macro::TokenStream;
+use quote::quote;
+// syn - разбирает код на rust в структуру данных которую мы можем менять
+// пример структуры разобранного кода:
+// DeriveInput {
+//     // --snip--
+//     ident: Ident {
+//         ident: "Pancakes",
+//         span: #0 bytes(95..103)
+//     },
+//     data: Struct(
+//         DataStruct {
+//             struct_token: Struct,
+//             fields: Unit,
+//             semi_token: Some(
+//                 Semi
+//             )
+//         }
+//     )
+// }
+
+// quote - собирает обратно из структуры даннх в код на Rust
+#[proc_macro_derive(HelloMacro)]
+pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+    impl_hello_macro(&ast)
+}
+
+fn impl_hello_macro(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl HelloMacro for #name {
+            fn hello_macro() {
+                println!("Hello, Macro! My name is {}!", stringify!(#name));
+            }
+        }
+    };
+    gen.into()
+}
