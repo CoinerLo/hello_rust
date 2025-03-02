858c1ae52830ab05c9b14dee669ad6ac99ed30a6
CoinerLo <coiner4677@gmail.com>
2025-03-02 14:51:03 +0300

    update hello_macro_derive v2

--- hello_macro/hello_macro_derive/src/lib.rs
+++ hello_macro/hello_macro_derive/src/lib.rs
@@ -41,8 +41,21 @@ fn impl_hello_macro(ast: &syn::DeriveInput) -> TokenStream {
 // Еще один типа макроса - атрибутный. Он используется самостоятельно и отдельно от derive
 // может использоваться не только со структурами и перечислениями но и с функциями либо другими типами
 // attr - аттрибут который прикрепляется, item - итем к которому прикрепляется аттрибут (на примере это функция)
+
+// пример макроса:
 // #[proc_macro_attribute]
 // pub fn route(attr: TokenStream, item: TokenStream) -> TokenStream {}
 
+// пример вызова:
 // #[route(GET, "/")]
 // fn index() {}
+
+
+// третий тип макросов - похожие на функции
+// можно объединить только с синтаксисом подобного сопоставлению
+// пример макроса:
+// #[proc_macro]
+// pub fn sql(input: TokenStream) -> TokenStream {}
+
+// пример вызова:
+// let sql = sql!(SELECT * FROM posts WHERE id=1);
