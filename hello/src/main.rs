5239c1becc344e273024a665a10d508fbd25b5cd
CoinerLo <coiner4677@gmail.com>
2025-03-04 21:08:01 +0300

    update hello v2

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::{BufRead, BufReader}, net::{TcpListener, TcpStream}};
+use std::{io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -16,5 +16,7 @@ fn handle_connection(mut stream: TcpStream) {
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
-    println!("Request: {http_request:#?}");
+    
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write_all(response.as_bytes()).unwrap();
 }
