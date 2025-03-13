d9938ae7c70d075039c06dc0766b7e03568a209d
CoinerLo <coiner4677@gmail.com>
2025-03-13 18:04:26 +0300

    update messenger_server

--- messenger_server/src/main.rs
+++ messenger_server/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    println!("Сервер запущен на 127.0.0.1:8080");
+
+    loop {
+        
+    }
 }
