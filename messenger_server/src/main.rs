ddea7fd83711f53e16574c320c42726790f72631
CoinerLo <coiner4677@gmail.com>
2025-03-14 20:26:40 +0300

    update messenger_server v2

--- messenger_server/src/main.rs
+++ messenger_server/src/main.rs
@@ -7,6 +7,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Сервер запущен на 127.0.0.1:8080");
 
     loop {
+        let n = match socket.read(&mut buffer).await {
+            Ok(n) if n == 0 => return,
+            Ok(n) => n,
+            Err(e) => {
+                eprintln!("Ошибка чтения: {}", e);
+                return;
+            }
+        };
         
     }
 }
