9c9c66f3b4e0af455ead1027ab10fc9ff3fe4814
CoinerLo <coiner4677@gmail.com>
2025-03-23 10:43:06 +0300

    update messenger_server v22

--- messenger_server/src/main.rs
+++ messenger_server/src/main.rs
@@ -46,106 +46,104 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 tokio::select! {
                     // чтение данных от клиента
                     result = socket.read(&mut buffer) => {
-                        // Читаем данные из сокета
-                        let n = match result {
-                            Ok(n) if n == 0 => {
-                                // Клиент закрыл соединение
-                                println!("Клиент {} отключился", addr);
-                                return;
+                        match result {
+                            // клиент отключился
+                            Ok(n) if n == 0 => break,
+                            // Успешно прочитано n байт
+                            Ok(n) => {
+                                // Преобразуем байты в строку
+                                let message_str = match String::from_utf8(buffer[..n].to_vec()) {
+                                    Ok(s) => s,
+                                    Err(_) => {
+                                        eprintln!("Ошибка декодирования utf-8");
+                                        continue;
+                                    }
+                                };
+
+                                // Десериализуем JSON в структуру Message
+                                let message: Message = match serde_json::from_str(&message_str) {
+                                    Ok(msg) => msg,
+                                    Err(e) => {
+                                        eprintln!("Ошибка десериализации JSON: {}", e);
+                                        continue;
+                                    }
+                                };
+
+                                // Обрабатываем сообщение
+                                match message {
+                                    Message::Join { username: new_username } => {
+                                        println!("Клиент {} клиент пытается присоединиться", new_username);
+
+                                        // Проверяем свободно ли имя
+                                        let mut clients_lock = clients.lock().await;
+                                        if clients_lock.contains_key(&new_username) {
+                                            drop(clients_lock);
+
+                                            let error_message = Message::ErrorMessage {
+                                                error: format!("Имя {} уже занято", new_username),
+                                            };
+                                            send_massage(&mut socket, &error_message).await;
+                                            break;
+                                        }
+
+                                        // добавляем клиента в список
+                                        clients_lock.insert(new_username.clone(), tx.clone());
+                                        drop(clients_lock);
+
+                                        username = Some(new_username.clone());
+
+                                        let response = Message::ReceiveMessage {
+                                            sender: "Server".to_string(),
+                                            content: format!("Добро пожаловать {}!", new_username),
+                                        };
+                                        send_massage(&mut socket, &response).await;
+                                    }
+                                    Message::SendMessage { content } => {
+                                        if let Some(sender) = &username {
+                                            println!("Получено сообщение от {}: {}", sender, content);
+                                            let message = serde_json::to_string(&Message::ReceiveMessage {
+                                                sender: sender.clone(),
+                                                content: content.clone(),
+                                            })
+                                            .unwrap();
+
+                                            if let Err(e) = tx.send(message) {
+                                                eprintln!("Ошибка отправки в канал: {}", e);
+                                            }
+                                        }
+                                    }
+                                    Message::Leave => {
+                                        if let Some(sender) = &username {
+                                            println!("Клиент {} покидает чат", sender);
+
+                                            // оповещаем других участников о выходе клиента
+                                            let notification = Message::ReceiveMessage {
+                                                sender: "Server".to_string(),
+                                                content: format!("{} покинул чат", sender),
+                                            };
+                                            let notification_json = serde_json::to_string(&notification).unwrap();
+                                            tx.send(notification_json).unwrap();
+
+                                            // удаляем клиента из списка
+                                            let mut clients_lock = clients.lock().await;
+                                            clients_lock.remove(sender);
+                                            drop(clients_lock);
+
+                                            // завершение задачи для этого клиента
+                                            break;
+                                        }
+                                    }
+                                    _ => {}
+                                }
                             }
-                            Ok(n) => n, // Успешно прочитано n байт
                             Err(e) => {
                                 eprintln!("Ошибка чтения от клиента {}: {}", addr, e);
-                                return;
+                                break;
                             }
-                        };
-
-                        // Преобразуем байты в строку
-                        let message_str = match String::from_utf8(buffer[..n].to_vec()) {
-                            Ok(s) => s,
-                            Err(_) => {
-                                eprintln!("Ошибка декодирования utf-8");
-                                continue;
-                            }
-                        };
-
-                        // Десериализуем JSON в структуру Message
-                        let message: Message = match serde_json::from_str(&message_str) {
-                            Ok(msg) => msg,
-                            Err(e) => {
-                                eprintln!("Ошибка десериализации JSON: {}", e);
-                                continue;
-                            }
-                        };
-
-                        // Обрабатываем сообщение
-                        match message {
-                            Message::Join { username: new_username } => {
-                                println!("Клиент {} клиент пытается присоединиться", new_username);
-
-                                // Проверяем свободно ли имя
-                                let mut clients_lock = clients.lock().await;
-                                if clients_lock.contains_key(&new_username) {
-                                    drop(clients_lock);
-
-                                    let error_message = Message::ErrorMessage {
-                                        error: format!("Имя {} уже занято", new_username),
-                                    };
-                                    send_massage(&mut socket, &error_message).await;
-                                    break;
-                                }
-
-                                // добавляем клиента в список
-                                clients_lock.insert(new_username.clone(), tx.clone());
-                                drop(clients_lock);
-
-                                username = Some(new_username.clone());
-
-                                let response = Message::ReceiveMessage {
-                                    sender: "Server".to_string(),
-                                    content: format!("Добро пожаловать {}!", new_username),
-                                };
-                                send_massage(&mut socket, &response).await;
-                            }
-                            Message::SendMessage { content } => {
-                                if let Some(sender) = &username {
-                                    println!("Получено сообщение от {}: {}", sender, content);
-                                    let message = serde_json::to_string(&Message::ReceiveMessage {
-                                        sender: sender.clone(),
-                                        content: content.clone(),
-                                    })
-                                    .unwrap();
-
-                                    if let Err(e) = tx.send(message) {
-                                        eprintln!("Ошибка отправки в канал: {}", e);
-                                    }
-                                }
-                            }
-                            Message::Leave => {
-                                if let Some(sender) = &username {
-                                    println!("Клиент {} покидает чат", sender);
-
-                                    // оповещаем других участников о выходе клиента
-                                    let notification = Message::ReceiveMessage {
-                                        sender: "Server".to_string(),
-                                        content: format!("{} покинул чат", sender),
-                                    };
-                                    let notification_json = serde_json::to_string(&notification).unwrap();
-                                    tx.send(notification_json).unwrap();
-
-                                    // удаляем клиента из списка
-                                    let mut clients_lock = clients.lock().await;
-                                    clients_lock.remove(sender);
-                                    drop(clients_lock);
-
-                                    // завершение задачи для этого клиента
-                                    break;
-                                }
-                            }
-                            _ => {}
                         }
                     }
 
+                    // получаем сообщения из канала
                     result = rx.recv() => {
                         match result {
                             Ok(msg) => {
