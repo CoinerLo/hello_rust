ceb2fedfce2b394ce76ff553141cf950393581b5
CoinerLo <coiner4677@gmail.com>
2025-03-29 20:34:24 +0300

    update messenger_server v42

--- messenger_server/src/db.rs
+++ messenger_server/src/db.rs
@@ -1,5 +1,19 @@
 use tokio_postgres::{NoTls, Error};
+use tracing::{error};
 
+// подключение
 pub async fn connecto_to_db() -> Result<tokio_postgres::Client, Error> {
-    
+    let (client, connection) = tokio_postgres::connect("host=localhost dbname=chat_server user=postgres password=111", NoTls).await?;
+
+    // запустить задачу для обработки соединения
+    tokio::spawn(async move {
+        if let Err(e) = connection.await{
+            error!("Ошибка подключения к базе данных: {}", e);
+        }
+    });
+
+    Ok(client)
 }
+
+// сохранение сообщения
+
