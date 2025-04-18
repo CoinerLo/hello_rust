643a735da95b45033ad4497babf62d19202def24
CoinerLo <coiner4677@gmail.com>
2025-04-18 20:31:24 +0300

    update messenger_server v101

---
+++ messenger_client/src/services/api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const API_URL = ''
+
+export default {
+  register(username, password) {
+    return axios.post(`${API_URL}/register`, { username, password });
+  },
+}
