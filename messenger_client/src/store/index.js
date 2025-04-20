7aabfe0584949b98665c7f3c4a93de6946016b40
CoinerLo <coiner4677@gmail.com>
2025-04-20 18:15:52 +0300

    update messenger_server v107

--- messenger_client/src/store/index.js
+++ messenger_client/src/store/index.js
@@ -9,9 +9,23 @@ export default createStore({
     messanges: {},
   },
   mutations: {
-
+    setUser(state, user) {
+      state.user = user;
+    },
+    setToken(state, token) {
+      state.token = token;
+    },
+    setChats(state,chats) {
+      state.chats = chats;
+    },
+    addMessage(state, { chatId, message }) {
+      if (!state.messanges[chatId]) {
+        state.messanges[chatId] = [];
+      }
+      state.messanges[chatId].push(message);
+    },
   },
   actions: {
-    
+
   },
 });
