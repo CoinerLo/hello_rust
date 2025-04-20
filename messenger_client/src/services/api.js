5e269cec56709162f45683a4f53bf61732692c43
CoinerLo <coiner4677@gmail.com>
2025-04-20 20:47:20 +0300

    update messenger_server v109

--- messenger_client/src/services/api.js
+++ messenger_client/src/services/api.js
@@ -6,5 +6,22 @@ export default {
   register(username, password) {
     return axios.post(`${API_URL}/register`, { username, password });
   },
-  
+  login(username, password) {
+    return axios.post(`${API_URL}/authenticate`, { username, password });
+  },
+  sendMessage(chatId, content) {
+    return axios.post(`${API_URL}/send-message`, { chatId, content });
+  },
+  createGroupChat(name) {
+    return axios.post(`${API_URL}/create-group-chat`, { name });
+  },
+  addMemberToGroupChat(chatId, username) {
+    return axios.post(`${API_URL}/add-member-to-group-chat`, { chatId, username });
+  },
+  removeMemberFroumGroupChat(chatId, username) {
+    return axios.delete(`${API_URL}/remove-member-from-group-chat`, { chatId, username });
+  },
+  deleteGroupChat(chatId) {
+    return axios.delete(`${API_URL}/delete-group-chat`, { chatId });
+  },
 }
