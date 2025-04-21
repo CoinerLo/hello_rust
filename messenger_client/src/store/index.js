b7fd9004b4c3fd545926d0a5c61b92e6a2289bd7
CoinerLo <coiner4677@gmail.com>
2025-04-21 22:09:51 +0300

    update messenger_server v111

--- messenger_client/src/store/index.js
+++ messenger_client/src/store/index.js
@@ -38,6 +38,14 @@ export default createStore({
       const response = await api.getGroupChats();
       commit('setChats', response.data);
     },
+    async sendMessage({commit}, { chatId, content }) {
+      const response = await api.sendMessage(chatId, content);
+      commit('addMessage', { chatId, message: response.data });
+    },
+    async createGroupChat({ commit }, name) {
+      const response = await api.createGroupChat(name);
+      commit('setChats', [ ...this.state.chats, response.data ]);
+    },
     
   },
 });
