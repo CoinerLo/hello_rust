60d61f02e7566f2674bf1dae1899516325b8ec76
CoinerLo <coiner4677@gmail.com>
2025-04-22 20:20:10 +0300

    update messenger_server v112

---
+++ messenger_client/src/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Авторизация</h2>
+    <form @submit.prevent="login">
+      <input v-model="username" placeholder="Имя пользователя" required />
+      <input v-model="password" type="password" placeholder="Пароль" required />
+      <button type="submit">Войти</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  import { mapActions } from 'vuex';
+
+  
+</script>
