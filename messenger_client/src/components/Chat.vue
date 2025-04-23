1ab3f6e2db95963026910108ae0f4d7816030553
CoinerLo <coiner4677@gmail.com>
2025-04-23 20:32:15 +0300

    update messenger_server v117

---
+++ messenger_client/src/components/Chat.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Чат</h2>
+    <div v-for="message in messages" :key="message.id">
+      <strong>{{ message.sender }}</strong>{{ message.content }}
+    </div>
+    <form @submit.prevent="sendMessage">
+      <input v-model="newMessage" placeholder="Введите сообщение" required />
+      <button type="submit">Отправить</button>
+    </form>
+  </div>
+</template>
+
+
--- messenger_client/src/router/index.js
+++ messenger_client/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import RegisterForm from "@/components/RegisterForm.vue";
+import RegisterForm from "@/components/Login.vue";
+import RegisterForm from "@/components/Chat.vue";
 
 const routes = [
   { path: '/register', component: RegisterForm },
