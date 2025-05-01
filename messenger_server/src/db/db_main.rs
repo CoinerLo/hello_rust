b67e800f562d4e8d79c1c5be9122bd5d1b5ab476
CoinerLo <coiner4677@gmail.com>
2025-05-01 09:46:45 +0300

    update messenger_server v139

--- messenger_server/src/db/db_main.rs
+++ messenger_server/src/db/db_main.rs
@@ -1,37 +1,28 @@
 use std::env::{self};
-use bb8_postgres::PostgresConnectionManager;
-use bb8::Pool;
-use tokio_postgres::NoTls;
+use sqlx::PgPool;
 use dotenv::dotenv;
 use tracing::{info, error};
 use crate::types::{AppResult, ServerError};
 
 // подключение
-pub async fn create_db_pool() -> AppResult<Pool<PostgresConnectionManager<NoTls>>> {
-  dotenv().ok();
+pub async fn create_db_pool() -> AppResult<PgPool> {
+    dotenv().ok();
 
-  let database_url = env::var("DATABASE_URL")
-      .map_err(|e| {
-          error!("DATABASE_URL is not sen in .env file: {}", e);
-          ServerError::VarError(e.into())
-      })?;
-  info!("Подключение к базе данных: {}", database_url);
+    let database_url = env::var("DATABASE_URL")
+        .map_err(|e| {
+            error!("DATABASE_URL is not sen in .env file: {}", e);
+            ServerError::VarError(e.into())
+        })?;
+    info!("Подключение к базе данных: {}", database_url);
 
-  
-  let manager = PostgresConnectionManager::new_from_stringlike(database_url,NoTls)
-      .map_err(|e| {
-          error!("Ошибка создания менеджера соединений: {}", e);
-          ServerError::CreateManagerError
-      })?;
 
-  let pool = Pool::builder()
-      .build(manager)
-      .await
-      .map_err(|e| {
-          error!("Ошибка создания пула соединений: {}", e);
-          ServerError::PoolError
-      })?;
+    let pool = PgPool::connect(&database_url)
+        .await
+        .map_err(|e| {
+            error!("Ошибка создания пула соединений: {}", e);
+            ServerError::PoolError(e)
+        })?;
 
-  info!("Пул соединений успешно создан");
-  Ok(pool)
+    info!("Пул соединений успешно создан");
+    Ok(pool)
 }
