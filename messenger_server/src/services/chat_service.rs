bc3a0d04a7e15ecd18cb4abced36d1f5391ff86e
CoinerLo <coiner4677@gmail.com>
2025-05-03 13:36:35 +0300

    update messenger_server v148

--- messenger_server/src/services/chat_service.rs
+++ messenger_server/src/services/chat_service.rs
@@ -9,3 +9,12 @@ pub async fn create_group_chat(pool: &DbPool, name: &str, creator: &str) -> AppR
             ServerError::CreateGroupChatError
         })
 }
+
+pub async fn delete_group_chat(pool: &DbPool, chat_id: i32, requester: &str) -> AppResult<()> {
+    group_chat::delete(pool, chat_id, requester)
+        .await
+        .map_err(|e| {
+            error!("Ошибка удаления группового чата: {}", e);
+            ServerError::CreateGroupChatError
+        })
+}
