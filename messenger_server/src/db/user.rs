60b5dd027421f2be359427528ae9eb69dc37a492
CoinerLo <coiner4677@gmail.com>
2025-05-03 13:11:38 +0300

    update messenger_server v148

--- messenger_server/src/db/user.rs
+++ messenger_server/src/db/user.rs
@@ -1,5 +1,5 @@
-use bcrypt::{hash, verify, DEFAULT_COST};
-use tracing::{warn, info, error};
+use bcrypt::{hash, DEFAULT_COST};
+use tracing::{info, error};
 
 use crate::types::{AppResult, DbPool, ServerError};
 
@@ -28,25 +28,6 @@ pub async fn register(
             ServerError::BcryptError(e.into())
         })?;
 
-    // // Проверяем, что пользователь с таким именем не существует
-    // let rows = sqlx::query!(
-    //     "SELECT COUNT(*) FROM users WHERE username = $1",
-    //     username
-    // )
-    // .fetch_all(pool)
-    // .await
-    // .map_err(|e| {
-    //     error!("Ошибка поиска пользователя: {}", e);
-    //     ServerError::DatabaseError { context: "Ошибка поиска пользователя".to_string(), source: e }
-    // })?
-    // .;
-
-    // let count: i64 = rows[0].get(0);
-    // if count > 0 {
-    //     warn!("Пользователь с именем {} уже существует", username);
-    //     return Err(ServerError::UserExists);
-    // }
-
     sqlx::query!(
         "INSERT INTO users (username, password_hash) VALUES ($1, $2)",
         username,
@@ -62,45 +43,3 @@ pub async fn register(
     info!("Пользователь {} успешно зарегистрирован", username);
     Ok(())
 }
-
-// pub async fn authenticate(
-//     pool: &DbPool,
-//     username: &str,
-//     password: &str,
-// ) -> AppResult<bool> {
-//     let client = pool
-//         .get()
-//         .await
-//         .map_err(|e| {
-//             error!("Ошибка получения соединения из пула: {}", e);
-//             ServerError::DatabaseError(e.into())
-//         })?;
-
-//     // Получаем хеш пароля и базы
-//     let rows = client
-//         .query("SELECT password_hash FROM users WHERE username = $1", &[&username])
-//         .await
-//         .map_err(|e| {
-//             error!("Ошибка поиска пользователя в базе данных: {}", e);
-//             ServerError::DatabaseError { context: "Ошибка поиска пользователя в базе данных".to_string(), source: e }
-//         })?;
-
-//     if rows.is_empty() {
-//         warn!("Пользователь {} не найден", username);
-//         return Ok(false);
-//     }
-
-//     let password_hash: String = rows[0].get(0);
-//     let is_valid = verify(password, &password_hash)
-//         .map_err(|e| {
-//             error!("Ошибка проверки пароля: {}", e);
-//             ServerError::BcryptError(e)
-//         })?;
-//     if !is_valid {
-//         warn!("Неверный пароль для пользователя {}", username);
-//         return Ok(false);
-//     }
-
-//     info!("Пользователь {} успешно авторизован", username);
-//     Ok(true)
-// }
