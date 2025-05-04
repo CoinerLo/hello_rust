87d00a1da989d46f87a2842647a5a2d067280511
CoinerLo <coiner4677@gmail.com>
2025-05-04 16:33:28 +0300

    update messenger_client v2

--- messenger_client/src/components/DefaultPage.vue
+++ messenger_client/src/components/DefaultPage.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="start-page">
     <h1>Добро пожаловать в чат</h1>
-    <div>
-
+    <p>
+      Это современный мессенджер, где вы можете общаться с друзьями, создавать групповые чаты и обмениваться сообщениями в реальном времени.
+    </p>
+    <div class="actions">
+      <button @click="goToRegister" class="btn">Зарегистрироваться</button>
+      <button @click="goToLogin" class="btn">Войти</button>
     </div>
   </div>
 </template>
 
 <script>
   export default {
-    
+    methods: {
+      goToRegister() {
+        this.$router.push('/register');
+      },
+      goToLogin() {
+        this.$router.push('/login');
+      },
+    },
   }
 </script>
