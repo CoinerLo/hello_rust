90438a928db837ba15c31cc75b0936dd1cebb181
CoinerLo <coiner4677@gmail.com>
2025-05-06 21:22:13 +0300

    update messenger_client v6

--- messenger_client/src/router/index.js
+++ messenger_client/src/router/index.js
@@ -2,15 +2,17 @@ import { createRouter, createWebHistory } from "vue-router";
 import RegisterForm from "@/components/RegisterForm.vue";
 import LoginPage from "@/components/LoginPage.vue";
 import ChatPage from "@/components/ChatPage.vue";
+import DefaultPage from "@/components/DefaultPage.vue";
 
 const routes = [
+  { path: '/', component: DefaultPage },
   { path: '/register', component: RegisterForm },
   { path: '/login', component: LoginPage },
   { path: '/chat/:chatId', component: ChatPage },
 ];
 
 const router = createRouter({
-  history: createWebHistory,
+  history: createWebHistory(),
   routes,
 });
 
