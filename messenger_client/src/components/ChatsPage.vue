f0fd1696c7d0a791c06ef5bbc038b1f8222d3688
CoinerLo <coiner4677@gmail.com>
2025-05-17 21:30:31 +0300

    update messenger_client v17

---
+++ messenger_client/src/components/ChatsPage.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Список чатов</h1>
+    </div>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+    import { mapActions } from "vuex";
+
+    export default {
+        name: 'ChatsPage',
+        methods: {
+            ...mapActions(['fetchChats']),
+        },
+        mounted() {
+            this.fetchChats();
+        },
+    }
+</script>
+
+<style>
+    
+</style>
