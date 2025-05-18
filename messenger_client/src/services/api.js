6b69669707c00e9b1bff0d9ef0f66eecb9267da6
CoinerLo <coiner4677@gmail.com>
2025-05-18 11:59:51 +0300

    update messenger_client v18

--- messenger_client/src/services/api.js
+++ messenger_client/src/services/api.js
@@ -1,30 +1,34 @@
 import axios from "axios";
 
-const API_URL = 'http://127.0.0.1:8081'
+const baseURL = 'http://127.0.0.1:8081'
+
+const instance = axios.create({ baseURL });
+
+instance.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
 export default {
   register(username, password) {
-    return axios.post(`${API_URL}/register`, { username, password });
+    return instance.post(`/register`, { username, password });
   },
   login(username, password) {
-    return axios.post(`${API_URL}/authenticate`, { username, password });
+    return instance.post(`/authenticate`, { username, password });
   },
   sendMessage(chatId, content) {
-    return axios.post(`${API_URL}/send-message`, { chatId, content });
-  },
-  createGroupChat(name) {
-    return axios.post(`${API_URL}/create-group-chat`, { name });
+    return instance.post(`/send-message`, { chatId, content });
   },
   addMemberToGroupChat(chatId, username) {
-    return axios.post(`${API_URL}/add-member-to-group-chat`, { chatId, username });
+    return instance.post(`/add-member-to-group-chat`, { chatId, username });
   },
   removeMemberFroumGroupChat(chatId, username) {
-    return axios.delete(`${API_URL}/remove-member-from-group-chat`, { chatId, username });
-  },
-  deleteGroupChat(chatId) {
-    return axios.delete(`${API_URL}/delete-group-chat`, { chatId });
+    return instance.delete(`/remove-member-from-group-chat`, { chatId, username });
   },
   getGroupChats() {
-    return axios.get(`${API_URL}/chats`);
+    return instance.get(`/chats`);
+  },
+  createGroupChat(name, creator) {
+    return instance.post(`/chats`, { name, creator });
+  },
+  deleteGroupChat(chatId) {
+    return instance.delete(`/chats`, { chatId });
   },
 }
