5518e18020ce801228b916c7a9a1c6928aa008cf
CoinerLo <coiner4677@gmail.com>
2025-05-24 20:55:56 +0300

    update messenger_client v19

--- messenger_client/src/components/ChatsPage.vue
+++ messenger_client/src/components/ChatsPage.vue
@@ -9,12 +9,19 @@
         </form>
     </div>
     <div>
-        
+        <ol v-if="chats.length" class="chat-list">
+            <li v-for="chat in chats" :key="chat.id" class="chat-item">
+                <span>{{chat.name}}</span>
+                <button @click="openChat(chat.id)">Перейти в чат</button>
+                
+            </li>
+        </ol>
+        <p v-else>Нет доступных чатов.</p>
     </div>
 </template>
 
 <script>
-    import { mapActions } from "vuex";
+    import { mapActions, mapState } from "vuex";
 
     export default {
         name: 'ChatsPage',
@@ -23,6 +30,9 @@
                 username: '',
                 chatName: '',
             };
+        },
+        computed: {
+            ...mapState(['chats']),
         },
         methods: {
             ...mapActions(['fetchChats', 'createGroupChat']),
@@ -41,6 +51,34 @@
     }
 </script>
 
-<style>
-    
+<style scoped>
+    .chat-list {
+        max-width: 600px;
+        margin: 0 auto;
+    }
+
+    .chat-item {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 10px;
+        border-bottom: 1px solid #ccc;
+    }
+
+    .chat-item:last-child {
+        border-bottom: none;
+    }
+
+    button {
+        padding: 5px 10px;
+        background-color: #007bff;
+        color: white;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+    }
+
+    button:hover {
+        background-color: #0056b3;
+    }
 </style>
