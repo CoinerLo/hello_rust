8014cda340d5e487b2f2c5ff7d552c18ac26f810
CoinerLo <coiner4677@gmail.com>
2025-06-01 18:26:02 +0300

    update guess_the_number

--- games/guess_the_number/src/main.rs
+++ games/guess_the_number/src/main.rs
@@ -1,7 +1,17 @@
 use rand::Rng;
+use std::io;
 
 fn main() {
     println!("Добро пожаловать в игру 'Угадай число'!");
 
     let secret_number = rand::rng().random_range(1..=100);
+
+    loop {
+        println!("Пожалуйста, введите ваше предположение:");
+
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Неудалось прочитать строку!");
+    }
 }
