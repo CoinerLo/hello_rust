97160b43b60d4c347952c2a748ed9061462417f2
CoinerLo <coiner4677@gmail.com>
2025-06-01 21:09:31 +0300

    update guess_the_number v4

--- games/guess_the_number/src/main.rs
+++ games/guess_the_number/src/main.rs
@@ -5,6 +5,24 @@ use std::cmp::Ordering;
 fn main() {
     println!("Добро пожаловать в игру 'Угадай число'!");
 
+    println!("Выберите уровень сложности:");
+    println!("1. Легкий (Диапазон 1-50 включительно, 10 попыток)");
+    println!("2. Средний (Диапазон 1-100 включительно, 7 попыток)");
+    println!("3. Сложный (Диапазон 1-200 включительно, 5 попыток)");
+
+    let mut difficulty = String::new();
+    io::stdin()
+        .read_line(&mut difficulty)
+        .expect("Выберите 1, 2 или 3");
+
+    let difficulty: u32 = match difficulty.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Необходимо выбрать 1, 2 или 3");
+            return;
+        }
+    }
+
     let secret_number = rand::rng().random_range(1..=100);
 
     loop {
