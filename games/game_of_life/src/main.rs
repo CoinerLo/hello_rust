4134ccf44225f6503be60d9b79e2e11d192256bf
CoinerLo <coiner4677@gmail.com>
2025-06-05 21:26:51 +0300

    update game_of_life

--- games/game_of_life/src/main.rs
+++ games/game_of_life/src/main.rs
@@ -1,3 +1,17 @@
+use std::fmt;
+
+#[derive(Clone, Copy, PartialEq)]
+enum Cell {
+    Alive,
+    Dead,
+}
+
+impl fmt::Disply for Cell {
+    fn fmt(&self, f: &mut fmt::Formater<'_>) -> fmt::Result {
+        write!(f, "{}", if *self == Cell::Alive { "■" } else { " " })
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
