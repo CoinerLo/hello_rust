8d03807fe5664e71ff5e4c1d3a62fbffd36c8982
CoinerLo <coiner4677@gmail.com>
2025-11-20 15:59:25 +0300

    create mini_string_perser

---
+++ WASM/mini_string_parser/src/main.rs
@@ -0,0 +1,19 @@
+enum Token {
+
+}
+
+enum Expr {
+    
+}
+
+fn tokenize(input: &str) -> Vec<Token> {
+    todo!()
+}
+
+fn parse(input: &str) -> Expr {
+    todo!()
+}
+
+fn main() {
+    println!("Hello, world!");
+}
