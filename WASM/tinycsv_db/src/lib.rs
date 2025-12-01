8622268f8f18a9a7766a8bedd9a8ed5ba4b86910
CoinerLo <coiner4677@gmail.com>
2025-12-01 21:14:57 +0300

    update tinycsv_db v4

--- WASM/tinycsv_db/src/lib.rs
+++ WASM/tinycsv_db/src/lib.rs
@@ -1,8 +1,23 @@
-pub enum Type {
-    Integer,
-    Text,
-    Float,
-    Boolean,
+use std::collections::HashMap;
+
+pub mod data_type {
+    #[derive(Debug, Clone, PartialEq)]
+    pub enum DataType {
+        Integer,
+        Text,
+        Float,
+        Boolean,
+    }
+
+    pub const Integer: DataType = DataType::Integer;
+    pub const Text: DataType = DataType::Text;
+    pub const Float: DataType = DataType::Float;
+    pub const Boolean: DataType = DataType:Boolean;
+}
+
+pub struct Schema {
+    pub fn colums: Vec<String, data_type::DataType>,
+    column_index: HashMap<String, usize>,
 }
 
 pub struct Row (String, Type);
@@ -17,6 +32,10 @@ pub fn database(schema: Vec<Row>) -> Database {
 
 pub fn insert_to(db: &mut Database, row: Row) {
     db.memory.push(row);
+}
+
+pub fn find_exact(db: &mut Database, name_col: str, value: ) {
+    
 }
 
 #[cfg(test)]
