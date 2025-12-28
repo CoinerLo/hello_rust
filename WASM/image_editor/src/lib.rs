dc2d0978cd6ead97439ef91e46da1b4269673588
CoinerLo <coiner4677@gmail.com>
2025-12-28 11:38:06 +0300

    update image_editor v1

--- WASM/image_editor/src/lib.rs
+++ WASM/image_editor/src/lib.rs
@@ -1,3 +1,20 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+mod image;
+mod lanczos;
+
+use wasm_bindgen::prelude::*;
+
+use image::Image;
+
+#[wasm_bindgen]
+pub fn wasm_resize(
+    data: &[u8],
+    src_width: usize,
+    src_height: usize,
+    dst_width: usize,
+    dst_height: usize,
+    r: usize
+) -> Vec<u8> {
+    let src_image = Image::from(data, src_width, src_height);
+
+    lanczos::resize(&src_image, dst_width, dst_height, r).data
 }
