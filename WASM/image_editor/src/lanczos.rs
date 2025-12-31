dd1dccd583f77b7cbac6c95260f8eea7f2ed1eb1
CoinerLo <coiner4677@gmail.com>
2025-12-31 12:32:21 +0300

    update image_editor v4

---
+++ WASM/image_editor/src/lanczos.rs
@@ -0,0 +1,10 @@
+use crate::image::Image;
+
+pub fn resize(src: &Image, dst_width: usize, dst_height: usize, r: usize) -> Image {
+    let mut dst = Image::new(dst_width, dst_height);
+    dst
+
+}
+
+
+
